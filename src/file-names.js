f418ab016d5755b8c86b073246e2743cc5e0fc49
IDemidova <irina.demidova.irina@gmail.com>
2022-04-24 20:25:10 +0300

    feat: add solution for file-names task

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(filesNames) {
+  if (filesNames.length == 0) {
+    return filesNames;
+  }
+
+  function findUniqueNames(filesNames) {
+    let uniqueNames = {};
+    for (let i = 0; i < filesNames.length; i++) {
+      if (!(filesNames[i] in uniqueNames)) {
+        uniqueNames[filesNames[i]] = [i];
+      } else {
+        uniqueNames[filesNames[i]].push(i);
+      }
+    }
+    return uniqueNames;
+  }
+
+  function changeFilesNames(filesNames, uniqueNames) {
+    for (let name in uniqueNames) {
+      for (let i = 0; i < uniqueNames[name].length; i++) {
+        if (i != 0) {
+          filesNames[uniqueNames[name][i]] = `${filesNames[uniqueNames[name][i]]}(${i})`;
+        }
+      }
+    }
+    return filesNames;
+  }
+
+  let uniqueNames = findUniqueNames(filesNames);
+
+  if (Object.keys(uniqueNames).length == filesNames.length) {
+    return filesNames;
+  }
+
+  if (Object.keys(uniqueNames).length != filesNames.length) {
+    while (Object.keys(uniqueNames).length < filesNames.length) {
+      filesNames = changeFilesNames(filesNames, uniqueNames);
+      uniqueNames = findUniqueNames(filesNames);
+    }
+    return filesNames;
+  }
 }
 
 module.exports = {
