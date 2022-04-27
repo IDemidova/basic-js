fb51fbca7d5a59b7e9e02925704b40a0a0d82f4d
IDemidova <irina.demidova.irina@gmail.com>
2022-04-27 21:09:27 +0300

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str != 'string') {
+    if (str == null || str == undefined) {
+      str = `${str}`
+    } else if (str[Symbol.toPrimitive]) {
+      str = 'STRING_OR_DEFAULT';
+    } else if (str.toString) {
+      str = str.toString();
+    }
+  }
+
+  if (Object.keys(options).length == 0) {
+    return str;
+  }
+
+  options = Object.assign({ separator: '+', additionSeparator: '|' }, options);
+
+  let stringToRepeat = '';
+
+  let additionToRepeat = [];
+
+  if (options.hasOwnProperty('addition')) {
+    if (typeof options.addition != 'string') {
+      if (options.addition == null || str == undefined) {
+        options.addition = `${options.addition}`
+      } else if (options.addition[Symbol.toPrimitive]) {
+        options.addition = 'STRING_OR_DEFAULT';
+      } else if (options.addition.toString) {
+        options.addition = options.addition.toString();
+      }
+    }
+    if (options.additionRepeatTimes) {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        additionToRepeat.push(options.addition);
+      }
+      additionToRepeat = additionToRepeat.join(`${options.additionSeparator}`);
+    } else {
+      additionToRepeat = options.addition;
+    }
+    stringToRepeat = `${str}${additionToRepeat}`;
+  } else {
+    stringToRepeat = str;
+  }
+
+  let repeatedString = [];
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      repeatedString.push(stringToRepeat);
+    }
+    repeatedString = repeatedString.join(`${options.separator}`);
+  } else {
+    repeatedString = stringToRepeat;
+  }
+
+  return repeatedString;
 }
 
 module.exports = {
